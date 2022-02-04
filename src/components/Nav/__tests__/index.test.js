fb6088aca7420de5352c4b05b52fb2333b488c37
kcheykim <kosalcheykim@gmail.com>
2022-02-03 19:07:24 -0800

    adding the contactForm to work

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,6 +8,8 @@ const categories = [
 ]
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -17,7 +19,9 @@ describe('Nav component', () => {
         render(<Nav
             categories={categories}
             setCurrentCategory={mockSetCurrentCategory}
-            currentCategory={mockCurrentCategory} />);
+            currentCategory={mockCurrentCategory} 
+            contactSelected={mockContactSelected}
+            setContactSelected={mockSetContactSelected}/>);
     })
 
     // snapshot test
@@ -37,7 +41,9 @@ describe('Nav component', () => {
             const { getByLabelText } = render(<Nav
                 categories={categories}
                 setCurrentCategory={mockSetCurrentCategory}
-                currentCategory={mockCurrentCategory} />)
+                currentCategory={mockCurrentCategory} 
+                contactSelected={mockContactSelected}
+                setContactSelected={mockSetContactSelected}/>)
             expect(getByLabelText('camera')).toHaveTextContent('📸');
         })
     })
@@ -47,7 +53,9 @@ describe('Nav component', () => {
             const { getByTestId } = render(<Nav
                 categories={categories}
                 setCurrentCategory={mockSetCurrentCategory}
-                currentCategory={mockCurrentCategory} />)
+                currentCategory={mockCurrentCategory} 
+                contactSelected={mockContactSelected}
+                setContactSelected={mockSetContactSelected}/>)
             expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
             expect(getByTestId('about')).toHaveTextContent('About me');
         })
